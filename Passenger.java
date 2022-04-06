2c254b43de1918ce452e9c09af8609ac94b830e2
kongress.ru <99484079+DanielElmirbekov@users.noreply.github.com>
2022-04-06 15:03:27 +0300

    Add files via upload

---
+++ Passenger.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Passenger {
+    private static int neededFloor;
+
+    public static int getNeededFloor() {
+        Passenger.neededFloor = Util.random(Dispatcher.getFloorQuant(), 1);
+        return neededFloor;
+    }
+}
---
+++ Util.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Util {
+    public static int random(int max, int min) {
+        return (int) (Math.random() * ((max - min) + 1)) + min;
+    }
+}
